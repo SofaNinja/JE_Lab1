04984ad8e977a3d987b5be2ff196f4cfc2aa093d
SofaNinja <nikitakuzneczov2004@gmail.com>
2024-12-04 14:01:59 +0200

    update switch case

--- src/main/java/org/example/patterns/CarFactory.java
+++ src/main/java/org/example/patterns/CarFactory.java
@@ -7,15 +7,11 @@ import org.example.patterns.bodytypes.Truck;
 
 public class CarFactory {
     public static Car createCar(String type, String name, double price, int year) {
-        switch (type) {
-            case "Sedan":
-                return new Sedan(name, price, year);
-            case "SUV":
-                return new SUV(name, price, year);
-            case "Truck":
-                return new Truck(name, price, year);
-            default:
-                throw new IllegalArgumentException("Unknown car type: " + type);
-        }
+        return switch (type) {
+            case "Sedan" -> new Sedan(name, price, year);
+            case "SUV" -> new SUV(name, price, year);
+            case "Truck" -> new Truck(name, price, year);
+            default -> throw new IllegalArgumentException("Unknown car type: " + type);
+        };
     }
 }
